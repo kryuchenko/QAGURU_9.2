e9ab87fee35323f197d62907dba7cde6baa330f0
Andrey Kryuchenko <andrey1984@gmail.com>
2023-11-15 21:25:38 +0200

    Update test_9_2.py

--- test_9_2.py
+++ test_9_2.py
@@ -26,8 +26,6 @@ def test_google_search(setup_browser):
     search_field.press_enter()
     search_results = browser.element('#search')
     search_results.should(have.text('test'))
-    search_results.matching(have.text('test'))
-
 
 def test_google_search_invalid_result(setup_browser):
     browser.open('http://google.com')
@@ -38,7 +36,4 @@ def test_google_search_invalid_result(setup_browser):
     search_field.should(be.visible).type("ывпывпывп3п3пп33п3п")
     time.sleep(1)
     search_field.press_enter()
-
-    no_results_text = browser.element("//p[contains(text(), 'Your search did not match any documents.')]")
-    if not no_results_text.matching(be.visible):
-        raise Exception("Error: search results are not empty")
+    browser.element('//*[@id="result-stats"]').should(have.text('About 0 results'))
